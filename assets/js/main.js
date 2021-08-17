354c92381c7148d2fcdb98f143f97b43ffe90c71
uxvu1196 <uxvu1196@gmail.com>
2021-08-17 22:56:34 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+/* Aufgabenstellung
+
+Bitte erstelle Variablen mit deinen persönlichen Daten (Name, Nachname, Alter, Geburtsort, Größe, Gewicht, Hobbys, Computerspiele, Lieblingsessen, Lieblingssport, Lieblingsmodemarke, Lieblingsjahreszeit, Lieblingsurlaubsort). Bilde einen Satz mit Hilfe von: 
+
+String concatenation: "string text " + let + " string text"
+Template literals: `string text ${expression} string text`
+
+
+Gib mit Hilfe von String concatenation oder Template literals einen Satz in der Konsole und in der HTML aus, der diese Variablen nutzt.
+ */
+
+let name = "Uyen";
+let lastName = "Vu";
+let age = 24;
+let birthPlace = "München";
+let height = "1.60 m"
+let hobbys = "exploring new restaurants, bars and places, travelling, netflix and more"
+let games = "sometimes I like to play on my ps4 and often on my phone I like to play Sudoku"
+let sport = "jogging, pole, hiking, gym"
+let favFood = "asian food, italian food, actually everything"
+let favBrand = "everything what suits me"
+let favTime = "Summer"
+let favHolidayPlace ="Greece, Spain, Vietnam, Croatia, Amsterdam, actually every country has their beautiful side"
+
+
+
+
